ad486ea9ac7cefa594a61a113122afca8255aa77
Bryan Apellanes <bryan@threeheadz.com>
2019-02-05 21:49:15 -0800

    wip: markdig components

--- shared/bam.net.shared/Presentation/Components/ComponentParser.cs
+++ shared/bam.net.shared/Presentation/Components/ComponentParser.cs
@@ -4,18 +4,66 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Bam.NMet.Presentation.Components
+namespace Bam.Net.Presentation.Components
 {
     public class ComponentParser : InlineParser
     {
-        public ComponentParser()
+        public ComponentParser() { }
+        public ComponentParser(IComponentResolver componentResolver)
         {
-            OpeningCharacters = new char[] { '`' };
+            OpeningCharacters = new char[] { '!' };
+            ComponentResolver = componentResolver;
         }
+
+        public IComponentResolver ComponentResolver { get; set; }
 
         public override bool Match(InlineProcessor processor, ref StringSlice slice)
         {
-            throw new NotImplementedException();
+            // [!clientId:NameOfComponent(- or |)] # comment
+            // ```
+            // init: function(component) {}
+            // click: function(component) {}
+            // mouseover: function(component){}
+            // ... other event handlers etc ...
+            // ```
+            bool match = false;
+            char openSquareBracket = slice.PeekCharExtra(-1);
+            char shouldBeWhitespace = slice.PeekCharExtra(-2);
+            if(shouldBeWhitespace.IsWhiteSpaceOrZero() && openSquareBracket == '[')
+            {
+                slice.NextChar();
+                StringSlice clientId = ReadUntil(slice, ':');
+                StringSlice nameOfComponent = ReadUntil(slice, ']');
+                string nameOfComponentString = nameOfComponent.ToString();
+                bool block = true;
+                if (nameOfComponentString.EndsWith("|"))
+                {
+                    nameOfComponentString = nameOfComponentString.Truncate(1);
+                    block = false;
+                }
+                if (ComponentResolver.IsValidComponentName(nameOfComponentString, out object component))
+                {
+                    
+
+                    match = true;
+                }
+            }
+
+            return match;
+        }
+
+        protected StringSlice ReadUntil(StringSlice slice, char until)
+        {
+            char current = slice.CurrentChar;
+            int start = slice.Start;
+            int end = start;
+            slice.NextChar();
+            while(current != until || current.IsWhiteSpaceOrZero())
+            {
+                end = slice.Start;
+                current = slice.NextChar();
+            }
+            return new StringSlice(slice.Text, start, end);
         }
     }
 }
---
+++ shared/bam.net.shared/Presentation/Components/IComponentResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bam.Net.Presentation.Components
+{
+    public interface IComponentResolver
+    {
+        bool IsValidComponentName(string componentName, out object component);
+    }
+}
